f4b2e0f472d5452a0794fa9928d46ac3d2ece00f
manato-takahashi <manato.takahashi86@gmail.com>
2023-12-27 02:21:22 +0900

    add: 4. Zigzag Conversionにもっと早く動作する修正コードを追記

--- 4. Zigzag Conversion.cs
+++ 4. Zigzag Conversion.cs
@@ -61,3 +61,26 @@ public class Solution {
     return string.Join("", answer);
     }
 }
+
+
+
+
+// GPT-4による自動生成コード（こっちの方が圧倒的に早い）
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (numRows == 1) return s;
+
+        StringBuilder sb = new StringBuilder();
+        int n = s.Length;
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                sb.Append(s[j + i]);
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    sb.Append(s[j + cycleLen - i]);
+            }
+        }
+        return sb.ToString();
+    }
+}
